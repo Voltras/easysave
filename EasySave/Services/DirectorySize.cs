fdbadc3ead906473fe561aea7e8960a3995bf3ed
anass-lrj <anass.laaraj@viacesi.fr>
2026-02-05 14:48:20 +0100

    ajout de la progressabr en backend

---
+++ EasySave/Services/DirectorySize.cs
@@ -0,0 +1,14 @@
+﻿namespace EasySave.Services;
+
+public static class DirectorySize
+{
+    public static long SizeInfo(this DirectoryInfo directory)
+    {
+        // On récupère la taille de tous les fichiers dans le dossier et les sous-dossiers
+        return directory.EnumerateFiles("*", SearchOption.AllDirectories)
+                        .Sum(file => file.Length);
+    }
+}
+
+   
+
